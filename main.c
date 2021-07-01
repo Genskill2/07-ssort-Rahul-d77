696b9ac2c8feafc4cee0e14e39abb2359cf05f4e
Rahul-d77 <84717074+Rahul-d77@users.noreply.github.com>
2021-07-01 10:59:35 +0530

    main.c

--- main.c
+++ main.c
@@ -50,26 +50,3 @@ int main(void) {
   test_swap_max();
   test_ssort();
   }
-
-void swap_max(int a[],int b, int c)
-{
-  int j,max=a[c];
-  for(int i=c;i<b;i++)
-  { 
-    if(a[i]>max)
-    {
-      max=a[i]; 
-      j=i;
-    }
-  }
-  a[j]=a[c];   
-  a[c]=max;     
-}
-
-void ssort(int d[],int e) 
-{
-  for(int i=0;i<e;i++) 
-  {
-    swap_max(d, e, i);
-  }
-}
