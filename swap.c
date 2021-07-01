c95416ac389605b913e00fff5b7778cb4e91814b
Rahul-d77 <84717074+Rahul-d77@users.noreply.github.com>
2021-07-01 15:06:11 +0530

    swap.c

--- swap.c
+++ swap.c
@@ -1,16 +1,19 @@
-void swap_max(int arr[], int l, int position){
-  int flag = 0; 
- int max = position;
-   for(int i = position ; i < l; i++){
-     if( arr[i] > arr[max] ){
+void swap_max(int a[], int b, int c)
+{
+  int j = 0; 
+  int max = c;
+  for(int i = c; i < b; i++)
+  {
+     if( a[i] > a[max] )
+      {
        max = i;
       }
-    }
-     flag = arr[max];
-     arr[max] = arr[position];
-     arr[position] = flag;
-  
    }
+     j = amax];
+     a[max] = a[c];
+     a[c] = j;
+   }
+
 void ssort(int d[],int e) 
 {
   for(int i=0;i<e;i++) 
