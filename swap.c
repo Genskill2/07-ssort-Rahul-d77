21665023024e8815eb94737bd467e88b99c2b6f7
Rahul-d77 <84717074+Rahul-d77@users.noreply.github.com>
2021-07-01 10:51:53 +0530

    swap.c

--- swap.c
+++ swap.c
@@ -1,16 +1,22 @@
+#include<stdio.h>
+
 void swap_max(int a[],int b, int c)
-{  
+{
+  
   int j,max=a[c];
   for(int i=c;i<b;i++)
   { 
-     if(a[i]>max)
+    
+    if(a[i]>max)
     {
-      max=a[i];  
+      max=a[i];   printf("max%i ",max);
       j=i;
     }
   }
-  a[j]=a[c];    
-  a[c]=max;    
+  a[j]=a[c];      printf(" a[j]%i",a[j]);
+  a[c]=max;       printf("  a[c]%i\n",a[c]);
+  
+  
 }
 
 void ssort(int d[],int e) 
@@ -18,5 +24,6 @@ void ssort(int d[],int e)
   for(int i=0;i<e;i++) 
   {
     swap_max(d, e, i);
+    for(int j=0;j<e;j++) printf("%i ",d[j]);printf("\n");
   }
 }
